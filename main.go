483f9de65f91f0ed91e5ac18278c5aede1c4a76e
devngho <56456663+devngho@users.noreply.github.com>
2020-09-24 07:02:23 +0900

    add mixed, sub

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package fraction
 
 type Fraction struct {
 	Bottom int
-	Top int
+	Top    int
 }
 
 func (f *Fraction) Add(other *Fraction)  {
@@ -19,8 +19,34 @@ func (f *Fraction) Add(other *Fraction)  {
 	}
 }
 
+func (f *Fraction) Subtraction(other *Fraction)  {
+	if f.Bottom != other.Bottom{
+		tmp := *f
+		r := *other
+		f.Top *= r.Bottom
+		f.Bottom *= r.Bottom
+		r.Top *= tmp.Bottom
+		r.Bottom *= tmp.Bottom
+		f.Top -= r.Top
+	}else{
+		f.Top -= other.Top
+	}
+}
+
+func (f *Fraction) MultiplicationWithInt(other int)  {
+	f.Top *= other
+}
+
+func (f *Fraction) Multiplication(other *Fraction)  {
+	//WIP
+}
+
 func (f *Fraction) Abbreviation() {
 	d := gcd(f.Top, f.Bottom)
 	f.Top /= d
 	f.Bottom /= d
+}
+
+func MixedToFraction(num int, fraction *Fraction) *Fraction {
+	return &Fraction{Bottom: fraction.Bottom, Top: num * fraction.Bottom + fraction.Top}
 }
